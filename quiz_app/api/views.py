316a177272e819a4b64cfc3aae01ea8e23a0d558
TobiasKlanert <tobias.klanert@t-online.de>
2025-12-06 16:13:10 +0100

    feat: quiz_app - The registered user can edit their own quiz using the PATCH method.

--- quiz_app/api/views.py
+++ quiz_app/api/views.py
@@ -52,3 +52,18 @@ class QuizDetailView(generics.RetrieveUpdateDestroyAPIView):
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
+
+    def patch(self, request, *args, **kwargs):
+        quiz = self.get_object()
+
+        serializer = self.get_serializer(
+            quiz,
+            data=request.data,
+            partial=True
+        )
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
